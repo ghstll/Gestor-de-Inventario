7ef10ca0b68233c4c699a2a1fda1f080300e7bd1
Braulio Compean <182840555+BraulioCompean@users.noreply.github.com>
2025-04-17 15:21:33 -0600

    saving

--- gestor-de-inventario/src/App.tsx
+++ gestor-de-inventario/src/App.tsx
@@ -1,42 +1,23 @@
 import { useState } from "react";
-import AsideMenu from "./components/ui/AsideMenu/AsideMenu";
-import GeneracionAutomaticaPage from "./components/ui/pages/GeneracionAutomaticaPage/GeneracionAutomaticaPage";
-import InicioPage from "./components/ui/pages/InicioPage/InicioPage";
-import OrdenesDeCompraPage from "./components/ui/pages/OperacionesPages/OrdenesDeCompra/OrdenesDeCompraPage";
+
+
+import { BrowserRouter } from "react-router-dom";
+
 export default function App() {
     const [darkMode, setDarkMode] = useState<boolean>(false);
 
     const [currentPage, setCurrentPage] = useState("GeneracionAutomatica");
 
-    const renderPage = () => {
-        console.log(currentPage);
-        switch (currentPage) {
-            case "Inicio":
-                return <InicioPage darkMode = {darkMode}></InicioPage>;
-            case "GeneracionAutomatica":
-                return <GeneracionAutomaticaPage></GeneracionAutomaticaPage>;
-            case "OrdenesDeCompra" : 
-                return <OrdenesDeCompraPage darkMode = {darkMode}></OrdenesDeCompraPage>
-            default:
-                return <h1>No encontrado</h1>;
-        }
-    };
 
     const toggleDarkMode = () => [setDarkMode(!darkMode)];
 
 
 
-
-    return (
-        <div className="flex box-border h-screen">
-            <AsideMenu toggleDarkMode={toggleDarkMode} darkMode = {darkMode} setCurrentPage={setCurrentPage}>
-
-            </AsideMenu>
-
-            <>
-                {renderPage()}
-            </>
-        </div>
+    return(
+        <>
+            <BrowserRouter>
+            </BrowserRouter>
+        </>
+    )
     
-    );
 }
---
+++ gestor-de-inventario/src/components/ui/pages/GeneracionAutomaticaPage/GeneracionAutomatica_Main/GeneracionAutomaticaMainPage.tsx
@@ -0,0 +1,19 @@
+import OrdenDeCompraIcon from "../../../../assets/GeneracionAutomatica/OrdenesDeCompraIcon.svg"
+import ReportIcon from "../../../../assets/GeneracionAutomatica/ReportIcon.svg"
+import GeneracionAutomaticaBtn from "../GeneracionAutomaticaBtn"
+export default function GeneracionAutomaticaMainPage({setPage} : {setPage : (page : "inicio" | "reportes" | "ordenes") => void}){
+    return(
+    <div className="border border-black w-[70%]  rounded-lg p-5">
+        <div className="p-5">
+            <h1 className="font-bold text-lg ml-10">
+                Ajustes de automatizacion
+            </h1>
+        </div>
+        <hr />
+        <section className="w-full h-fit flex flex-col items-center mt-10 gap-5">
+            <GeneracionAutomaticaBtn title="Reportes" description="Configura la automatizacion de reportes" onClickFunc={() => setPage("reportes")} icon = {ReportIcon} ></GeneracionAutomaticaBtn>
+            <GeneracionAutomaticaBtn title="Ordenes de compra" description="Configura la automatizacion de las ordenes de compra" icon = {OrdenDeCompraIcon}></GeneracionAutomaticaBtn>
+        </section>
+    </div>
+    )
+}
